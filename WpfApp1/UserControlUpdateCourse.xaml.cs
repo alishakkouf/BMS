4be860558da7a02755037cd02a91dd9411e8ea3d
ali shakkouf <alishakkouf404@gmail.com>
2020-09-21 10:18:38 +0300

    add backend code to update course

--- WpfApp1/UserControlUpdateCourse.xaml.cs
+++ WpfApp1/UserControlUpdateCourse.xaml.cs
@@ -20,14 +20,23 @@ namespace WpfApp1
     /// </summary>
     public partial class UserControlUpdateCourse : UserControl
     {
+        financialContext financialContext = new financialContext();
+        public int idForUpdate;
         public UserControlUpdateCourse()
         {
             InitializeComponent();
+            //course.ItemsSource = financialContext.course.ToList();
+            //course.SelectedValuePath = "Idemployee";
+            //course.DisplayMemberPath = "Name";
         }
 
         private void Course_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            //idForUpdate = int.Parse(course.SelectedValue.ToString());
+            //Employee empToUpdate = financialContext.Employee.FirstOrDefault(x => x.Idemployee == idForUpdate);
+            //List<Employee> empSelected = new List<Employee>();
+            //empSelected.Add(empToUpdate);
+            //allEmployee.ItemsSource = empSelected;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
